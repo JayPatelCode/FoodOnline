6b0e980e8da6b03877417128d2e8694d48db3340
JayPatelCode <pateljayparag@gmail.com>
2025-06-14 17:27:24 +0530

    Customer and Vendor registration

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,7 +1,25 @@
+########VENDOR#############
+# testrest@email.com
+# 123
+
+########CUSTOMER#########
+# jack@email.com       
+# Jaypatel312003#
+
+######logiN######
+# http://localhost:8000/accounts/login/
+
+
 from django.urls import path
 from . import views
 
 urlpatterns = [
     path("registerUser/", views.registerUser, name="registerUser"), 
-    path("registerVendor/", views.registerVendor, name="registerVendor")
+    path("registerVendor/", views.registerVendor, name="registerVendor"),
+
+    path("login/", views.login, name="login"),
+    path("logout/", views.logout, name="logout"),
+    path("myAccount/", views.myAccount, name="myAccount"),
+    path("custDashboard/", views.custDashboard, name="custDashboard"),
+    path("vendorDashboard/", views.vendorDashboard, name="vendorDashboard")
 ]
